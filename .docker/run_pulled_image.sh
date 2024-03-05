2587a5eb42530434bc1ebe6be307939b1f616547
Emily Sheetz <ejsheetz@yahoo.com>
2024-03-05 15:15:51 -0600

    install terminator, update run scripts with additional permissions for graphics

--- .docker/run_pulled_image.sh
+++ .docker/run_pulled_image.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# This run script starts the docker container with --network=host. This allows
+# ROS nodes running on the host to interact with ROS nodes running in the
+# container.
+# Note that running this way is not in itself sufficient -- the ROS environments
+# on host and container must also be compatible. This means:
+# - ROS_MASTER_URI must match
+# - DDS domain IDs must match if the host is starting ROS2/IHMC nodes
+
+# set up variables
 pulled_image_name="ghcr.io/esheetz/nstgro_research_docker"
 pulled_image_tag="feature-create-docker-image"
 container_name="punkrobot_nstgro"
@@ -7,11 +16,20 @@ nstgro_workspace_name="nstgro_ws"
 val_workspace_name="val_ws"
 helper_scripts_dir="workspace_helper_scripts_docker"
 
+# get this directory
 thisdir=$(dirname "$(readlink -f "$0")")
-# echo $thisdir
+
+# allow container to show GUIs
+xhost +si:localuser:root
 
 docker run -it                                                                              \
            --rm                                                                             \
+           --env="DISPLAY=$DISPLAY"                                                         \
+           --env="QT_X11_NO_MITSHM=1"                                                       \
+           --env="XAUTHORITY=$XAUTH"                                                        \
+           --volume="$XAUTH:$XAUTH"                                                         \
+           -e DISPLAY                                                                       \
+           -v /tmp/.X11-unix:/tmp/.X11-unix                                                 \
            -v ${thisdir}/../${val_workspace_name}/:/${val_workspace_name}/                  \
            -v ${thisdir}/../${nstgro_workspace_name}/:/${nstgro_workspace_name}/            \
            -v ${thisdir}/../${helper_scripts_dir}/:/${helper_scripts_dir}/                  \
